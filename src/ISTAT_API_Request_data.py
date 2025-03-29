1a1b5417a30aa78c4853f458a23a9286f3acb334
Davide-OttoneCasagrande <davide.8ne@gmail.com>
2025-03-29 10:39:33 +0000

    added Function docstring and standard arg to filter_122_54(), function docstring to filter()

--- src/ISTAT_API_Request_data.py
+++ src/ISTAT_API_Request_data.py
@@ -9,6 +9,15 @@ maxFilter = 35
 filterLocationIdJsonPath = os.path.join("data", "filter_location_id_rest_request.json")
 
 def filter(locationID_path:str=filterLocationIdJsonPath, maxFilter:int=maxFilter)-> list[str]:
+    """
+    Args:
+        locationID_path (str): Path to the JSON file with the structure: {"id": [i1, i2, ...]}.
+        maxFilter (int): Maximum number of filters for a single REST request (default: 35).
+
+    Returns:
+        list[str]: A list of strings, each containing concatenated IDs in the format:
+                   "i1+i2+...+in", "in1+in2+...+i2n", suitable for REST requests.
+    """
 
     with open(locationID_path,"r") as file:
         data = json.load(file)
@@ -28,7 +37,17 @@ def filter(locationID_path:str=filterLocationIdJsonPath, maxFilter:int=maxFilter
             current_filter= f"{fileData[I]}"
     return filters
 
-def filter_for_122_54(filters:list[str]) -> list[str]:
+def filter_for_122_54(filters:list[str]=filter()) -> list[str]:
+    """
+    Args:
+        filters (list[str]): A list of filter strings, each containing concatenated IDs
+                             in the format "i1+i2+...+in", suitable for REST requests.
+
+    Returns:
+        list[str]: A list of formatted strings specific to the '122_54' dataflow,
+                   where each string is wrapped with additional characters for the request.
+    """
+
     FilterString122_54:list[str] = []
     for filter in filters:
         FilterString122_54.append(f"...{filter}.......")
