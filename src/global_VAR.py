ff6cf5158cb757b1b57c26a2df4faf5a692bcbde
Davide-OttoneCasagrande <davide.8ne@gmail.com>
2025-04-14 13:25:35 +0200

    added easier language setting for global_VAR.py

--- src/global_VAR.py
+++ src/global_VAR.py
@@ -1,4 +1,5 @@
-# REST request ISTAT API data       #dataType:int = 0  # 0 = CSV  1 = JSON
+# REST request ISTAT API data
+language_is_ITA: bool = True
 max_num_filter:int = 18
 timeframe:str = 'startPeriod=2016-01-01'
 searchId:str = "ITC3" #codice per la liguria
@@ -12,7 +13,10 @@ dataflows = [
     ('122_54',"DCSC_TUR","facts_turism_raw"),
     ("161_268","DCSP_SBSREG","facts_indicatori_economici_raw")
 ]
-#folders path
-facts_FolderPath: dict[str, str] = {"root":"src", "path":"SQL_script", "name":"facts"}
-dim_FolderPath: dict[str, str] = {"root":"src", "path":"SQL_script", "name":"dim_ITA"}
+#folders pathS
 drop_script_path: dict[str, str] = {"root":"src", "path":"SQL_script", "name":"drop_all_views.sql"}
+facts_FolderPath: dict[str, str] = {"root":"src", "path":"SQL_script", "name":"facts"}
+if language_is_ITA:
+    dim_FolderPath: dict[str, str] = {"root":"src", "path":"SQL_script", "name":"dim_ITA"}
+else:
+    dim_FolderPath: dict[str, str] = {"root":"src", "path":"SQL_script", "name":"dim_ENG"}
